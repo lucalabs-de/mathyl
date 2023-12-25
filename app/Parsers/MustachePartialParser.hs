b2a14373f92c242c7909a02c8c535c99d24aea87
lucalabs-de <gspitzer@leoapp-slg.de>
2023-12-25 02:49:01 +0100

    implement parser to find mustache partials

--- app/Parsers/MustachePartialParser.hs
+++ app/Parsers/MustachePartialParser.hs
@@ -1,1 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Parsers.MustachePartialParser where
+
+import Control.Monad.Combinators (some)
+import Data.List (intercalate)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Void
+import Text.Megaparsec (Parsec, anySingle, many, noneOf, sepBy1, (<?>), (<|>))
+import Text.Megaparsec.Char 
+
+type Parser = Parsec Void Text
+
+pPartialBeg :: Parser String
+pPartialBeg = T.unpack <$> string "{{>" <* many (char ' ')
+
+pPartialEnd :: Parser String
+pPartialEnd = many (char ' ') *> (T.unpack <$> string "}}")
+
+pFileName :: Parser String
+pFileName = some (noneOf ['{', '}', '<', '>', '/'])
+
+pFilePath :: Parser String
+pFilePath =
+  concat3
+    <$> many (char '/')
+    <*> (intercalate "/" <$> sepBy1 pFileName (many (char '/')))
+    <*> many (char '/')
+      <?> "file path"
+
+pPartial :: Parser String
+pPartial = pPartialBeg *> pFilePath <* pPartialEnd
+
+pPartials :: Parser [String]
+pPartials = many loop
+  where loop = pPartial <|> (anySingle *> loop)
+
+concat3 :: String -> String -> String -> String
+concat3 x y z = x ++ y ++ z
