37b880b9d241fff5c4c184ac5ab860be4ffd84cd
lucalabs-de <gspitzer@leoapp-slg.de>
2024-11-18 23:06:22 +0100

    simplify math compiler

--- app/Compilers/MathCompiler.hs
+++ app/Compilers/MathCompiler.hs
@@ -8,35 +8,46 @@ import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Reader (MonadReader)
 import Data.Aeson (Value (..))
 import Data.ByteString (ByteString)
-import Data.Text (Text)
+import Data.Text (Text, replace)
 import Data.Text.Encoding (encodeUtf8)
 import Quickjs (JSContextPtr, eval, eval_, quickjs)
-import Text.Pandoc (Block (..), Format (..), Inline (..), MathType (..), Pandoc (..))
+import Text.Pandoc (Format (..), Inline (..), MathType (..), Pandoc (..))
 import Text.Pandoc.Walk
 
 import Util.Files (katexJs)
+
+data MathRenderType = Inline | Display
 
 processMathBlocks :: (MonadIO m, MonadThrow m, MonadMask m) => Pandoc -> m Pandoc
 processMathBlocks ast = quickjs $ do
   eval_ katexJs
 
-  flip handleMathBlocks ast $ \math -> do
-    mathHtml <- eval (mkFunctionCall math)
+  flip handleMathBlocks ast $ \math rdrMode -> do
+    mathHtml <- eval (mkFunctionCall math rdrMode)
     return $ RawInline (Format "html") (fromStringValue mathHtml)
 
-handleMathBlocks :: forall m. (MonadReader (JSContextPtr) m) => (Text -> m Inline) -> Pandoc -> m Pandoc
-handleMathBlocks f = walkM parseMath
+handleMathBlocks ::
+  forall m.
+  (MonadReader (JSContextPtr) m) =>
+  (MathRenderType -> Text -> m Inline) ->
+  Pandoc ->
+  m Pandoc
+handleMathBlocks f = walkM parseMath 
  where
-  parseMath :: Block -> m Block
-  parseMath = walkM replaceMath
+  parseMath :: Inline -> m Inline
+  parseMath (Math InlineMath str) = f Inline str
+  parseMath (Math DisplayMath str) = f Display str
+  parseMath p = return p
 
-  replaceMath :: Inline -> m Inline
-  replaceMath (Math InlineMath str) = f str
-  replaceMath (Math DisplayMath str) = f str
-  replaceMath p = return p
-
-mkFunctionCall :: Text -> ByteString
-mkFunctionCall m = "renderToString(\")" <> encodeUtf8 m <> "\")"
+mkFunctionCall :: MathRenderType -> Text -> ByteString
+mkFunctionCall Inline m =
+  "katex.renderToString(\""
+    <> encodeUtf8 (replace "\\" "\\\\" m)
+    <> "\")"
+mkFunctionCall Display m =
+  "katex.renderToString(\""
+    <> encodeUtf8 (replace "\\" "\\\\" m)
+    <> "\", { displayMode: true })"
 
 fromStringValue :: Value -> Text
 fromStringValue (String t) = t
