8ed70d51e37fac627b964ec8aae644ebe3552e08
lucalabs-de <gspitzer@leoapp-slg.de>
2024-11-19 03:22:24 +0100

    simplify pixel math

--- app/Util/MathHelpers.hs
+++ app/Util/MathHelpers.hs
@@ -1,8 +1,14 @@
 module Util.MathHelpers where
 
-scaleToHeight :: Double -> Double -> Double -> Double
-scaleToHeight newHeight oldHeight oldWidth = newHeight * (oldWidth / oldHeight)
+scaleToDimension :: Double -> Double -> Double -> Double
+scaleToDimension newRefDim oldRefDim oldDim = (newRefDim / oldRefDim) * oldDim
+
+scaleToPixelDimension :: Int -> Double -> Double -> Int
+scaleToPixelDimension newRefDim oldRefDim oldDim =
+  round $ scaleToDimension (fromIntegral newRefDim) oldRefDim oldDim
 
 scaleToPixelHeight :: Int -> Double -> Double -> Int
-scaleToPixelHeight newHeight oldHeight oldWidth =
-  round $ scaleToHeight (fromIntegral newHeight) oldHeight oldWidth
+scaleToPixelHeight = scaleToPixelDimension
+
+scaleToPixelWidth :: Int -> Double -> Double -> Int
+scaleToPixelWidth = scaleToPixelDimension
